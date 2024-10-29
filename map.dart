4d05798c2595b38f2bc20d04ca0368db0b8ac445
mimas9107 <mimas9107@gmail.com>
2024-10-29 12:23:29 +0800

    2024/10/29 dart程式碼教學1

---
+++ map.dart
@@ -0,0 +1,38 @@
+void main(List<String> args){
+
+  // map: 類似字典, key:value pair, ; 而 key不能重複.
+  // { key1:value1, key2:value2, ...}
+  
+  Map m1={'name':'John', 'age': 22};
+  var m2=Map();
+  var m3=Map.from({'name':'JoJo','age':19});
+
+  print("m1: ${m1}");
+  print("m2: ${m2}");
+  print("m3: ${m3}");
+
+  // add new element:
+  m1['course']="japanese";
+  print("m1: ${m1}");
+
+  // 取得所有 key:
+  print("m1.keys : ${m1.keys}");
+  print("m1.keys.toList() : ${m1.keys.toList()}");
+  // 取得所有 value:
+  print("m1.values : ${m1.values}");
+  print("m1.values.toList() : ${m1.values.toList()}");
+
+  // 一次塞多個 key:value addAll()
+  m1.addAll({'dept':'ce','email':'abc@xyz'});
+  print("m1: ${m1}");
+
+  // 刪除
+  m1.remove('name');
+  print("m1.remove(\'name\') m1: ${m1}");
+
+
+  // 走訪元素:
+  m1.forEach( (key,value)=>print("${key} : ${value}") );
+
+
+}
---
+++ samples/set.dart
@@ -0,0 +1,64 @@
+void main(){
+  // set := collection, 跟 List一樣有順序性, 但 元素值不可重複, 代表符號: {}
+  
+  // 宣告 並初始化:
+  Set s1={1,2,3,4,"d"};
+  var s2=Set();    // empty set object by class constructor
+  var s3=new Set();// empty set object
+
+  var s4=Set.from({1,2,3,4,"d"});
+
+  print("s1: ${s1}");
+  print("s2: ${s2}");
+  print("s3: ${s3}");
+  print("s4: ${s4}");
+
+  // 添加 新元素:
+  s2.add("abcd");
+  print("s2: ${s2}");
+  s3.addAll(s1);
+  print("s3: ${s3}");
+  s3.addAll(s1);
+  print("s3: ${s3}");
+
+
+
+  // 取值 要用 elementAt()方法:
+  print("s3.elementAt(3): ${s3.elementAt(3)}");
+
+  // 找 set中 某個元素值:
+  print("s3.contains(\"d\"): ${s3.contains("d")}");
+
+  // 刪除某個值:
+  s3.remove(4);
+  print("s3: ${s3}");
+
+  // 走訪每元素:
+  s1.forEach((element)=> print(element));
+
+  // set 轉 list:
+  List L1=s1.toList();
+  print('L1: ${L1}');
+
+  // ====================================
+  // set的 UNION, INTERSECT, DIFFERENCE
+  Set s5={1,2,3,4,5};
+  Set s6={1,2,10,12,28};
+  // 聯集 union()
+  print("s5.union(s6) : ${s5.union(s6)}");
+  // 交集 intersection()
+  print("s5.intersection(s6) : ${s5.intersection(s6)}");
+  // 差集 difference()
+  print("s5.difference(s6) : ${s5.difference(s6)}");
+  print("s6.difference(s5) : ${s6.difference(s5)}");
+
+
+  // ====================================
+  // list to set
+  List L2 = [1,2,2,2,3,4,5,6,3,2];
+  Set s7 =L2.toSet();
+  print("L2: ${L2} , toSet => s7 : ${s7}");
+
+  // 不能直接用 []索引去 asign set內元素:
+  //s7[2]=90; // X error!!
+}
